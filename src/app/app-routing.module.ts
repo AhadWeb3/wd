f14a9f23f54a84ac9add0d3a2d77ed4db8464054
Venkata Sai Pradeep Varma Nadimpalli <Pradeep.Nadimpalli@ivycomptech.com>
2023-08-20 10:15:56 +0530

    init: add splash screen
    Signed-off-by: Venkata Sai Pradeep Varma Nadimpalli <Pradeep.Nadimpalli@ivycomptech.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./../app/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: HomeComponent,
+    path: "marriage"
+  },
+  {
+    path: "",
+    redirectTo: "/marriage",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
